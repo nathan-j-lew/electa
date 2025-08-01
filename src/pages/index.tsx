483d2fe6a05d55c1ac8b9c4980284ca317919130
nathan-j-lew <72537484+nathan-j-lew@users.noreply.github.com>
2025-08-01 02:45:44 -0700

    fix: link to item accessibly
    Added invisible span for coarse pointer on link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { Libre_Bodoni } from "next/font/google";
 import { motion, useMotionValueEvent, useScroll } from "motion/react";
 import { useEffect, useMemo, useRef, useState } from "react";
+import Link from "next/link";
 
 const libreBodoni = Libre_Bodoni({
   variable: "--font-libre-bodoni",
@@ -12,22 +13,27 @@ const items = [
   {
     title: "Campo Grafico",
     hex: "#F4ECEA",
+    id: "campo",
   },
   {
     title: "Lo Studio Boggeri",
     hex: "#D74C2F",
+    id: "boggeri",
   },
   {
     title: "Max Huber",
     hex: "#D9AF01",
+    id: "huber",
   },
   {
     title: "Bruno Munari",
     hex: "#6A6E71",
+    id: "munari",
   },
   {
     title: "Hans Neuberg",
     hex: "#BC2729",
+    id: "neuberg",
   },
 ];
 
@@ -36,15 +42,15 @@ export default function Home() {
   const { scrollYProgress } = useScroll();
   const [currentItem, setCurrentItem] = useState(0);
   useMotionValueEvent(scrollYProgress, "change", (latest) => {
+    // console.log("latest", latest);
     const index = Math.floor(latest * items.length);
-    if (index >= items.length) return;
-    setCurrentItem(index);
+    setCurrentItem(index >= items.length ? items.length - 1 : index);
   });
 
   return (
     <div className={`${libreBodoni.className} font-sans`}>
       <main className="flex flex-col items-center" ref={ref}>
-        <section className="fixed inset-0 h-lvh">
+        <section className="fixed inset-x-0 top-0 h-svh">
           <div className="px-8 flex flex-col items-center justify-center gap-4 size-full">
             <h2 className="text-xl font-bold text-center w-full text-white bg-blend-difference">
               {items[currentItem].title}
@@ -54,11 +60,14 @@ export default function Home() {
               style={{ backgroundColor: items[currentItem].hex }}
             />
             <div className="h-4 border border-foreground w-[80vmin] flex relative max-w-[40rem]">
-              {Array.from({ length: items.length }).map((_, j) => (
-                <div
+              {items.map((_, j) => (
+                <Link
                   key={`item-${j}`}
-                  className={`flex-1 border border-foreground`}
-                />
+                  className={`flex-1 border border-foreground relative`}
+                  href={`#${items[j].id}`}
+                >
+                  <span className="absolute h-12 w-full -translate-1/2 top-1/2 left-1/2 any-pointer-fine:hidden" />
+                </Link>
               ))}
               <motion.div
                 className={`bg-foreground absolute h-full w-1/5`}
@@ -71,7 +80,7 @@ export default function Home() {
           <section
             key={`section--${i}`}
             className="w-full h-lvh min-h-[36rem]"
-            // style={{ backgroundColor: item.hex }}
+            id={item.id}
           />
         ))}
       </main>
