15b96ae22bf87a51293d4e3e5e004fbcb5983f81
nathan-j-lew <72537484+nathan-j-lew@users.noreply.github.com>
2025-07-31 21:09:55 -0700

    fix: bodoni placeholder

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,15 @@
 import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Libre_Bodoni } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const libreBodoni = Libre_Bodoni({
+  variable: "--font-libre-bodoni",
   subsets: ["latin"],
 });
 
 export default function Home() {
   return (
     <div
-      className={`${geistSans.className} ${geistMono.className} font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
+      className={`${libreBodoni.className} font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
     >
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
