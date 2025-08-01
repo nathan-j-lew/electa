532bfda78452db4262c119d168c629e23769be13
nathan-j-lew <72537484+nathan-j-lew@users.noreply.github.com>
2025-08-01 02:26:00 -0700

    feat: switch item on scroll

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Libre_Bodoni } from "next/font/google";
-import { motion } from "motion/react";
+import { motion, useMotionValueEvent, useScroll } from "motion/react";
+import { useEffect, useMemo, useRef, useState } from "react";
 
 const libreBodoni = Libre_Bodoni({
   variable: "--font-libre-bodoni",
@@ -31,28 +32,51 @@ const items = [
 ];
 
 export default function Home() {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll();
+  const [currentItem, setCurrentItem] = useState(0);
+  useMotionValueEvent(scrollYProgress, "change", (latest) => {
+    const index = Math.floor(latest * items.length);
+    if (index >= items.length) return;
+    setCurrentItem(index);
+  });
+
   return (
     <div className={`${libreBodoni.className} font-sans`}>
-      <main className="flex flex-col items-center ">
-        {items.map((item, i) => (
-          <section key={`section--${i}`} className="w-full h-lvh">
-            <div className="px-8 flex flex-col items-center justify-center gap-4 size-full">
-              <h2 className="text-xl font-bold">{item.title}</h2>
-              <div
-                className="aspect-square w-full max-w-[40rem]"
-                style={{ backgroundColor: item.hex }}
+      <main className="flex flex-col items-center" ref={ref}>
+        <motion.div
+          style={{ scale: scrollYProgress }}
+          className="bg-blue-500 size-8 rounded-full fixed top-4 left-4 z-50"
+        />
+        <section className="fixed inset-0 h-lvh">
+          <div className="px-8 flex flex-col items-center justify-center gap-4 size-full">
+            <h2 className="text-xl font-bold text-center w-full text-white bg-blend-difference">
+              {items[currentItem].title}
+            </h2>
+            <div
+              className="aspect-square size-[80vmin] max-w-[40rem]  bg-blend-difference"
+              style={{ backgroundColor: items[currentItem].hex }}
+            />
+            <div className="h-4 border border-foreground w-[80vmin] flex relative max-w-[40rem]">
+              {Array.from({ length: items.length }).map((_, j) => (
+                <div
+                  key={`item-${j}`}
+                  className={`flex-1 border border-foreground`}
+                />
+              ))}
+              <motion.div
+                className={`bg-foreground absolute h-full w-1/5`}
+                style={{ x: currentItem * 100 + "%" }}
               />
-              <div className="h-4 border border-foreground w-full flex relative">
-                {Array.from({ length: items.length }).map((_, j) => (
-                  <div
-                    key={`item-${j}`}
-                    className={`flex-1 border border-foreground`}
-                  />
-                ))}
-                <motion.div className={`bg-foreground absolute h-full w-1/5`} />
-              </div>
             </div>
-          </section>
+          </div>
+        </section>
+        {items.map((item, i) => (
+          <section
+            key={`section--${i}`}
+            className="w-full h-lvh min-h-[36rem]"
+            // style={{ backgroundColor: item.hex }}
+          />
         ))}
       </main>
     </div>
