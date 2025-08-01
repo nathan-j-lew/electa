e8c48cedaba868246a1b2e4d68829e7a698d91ff
nathan-j-lew <72537484+nathan-j-lew@users.noreply.github.com>
2025-08-01 02:01:29 -0700

    feat: initial layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,110 +1,60 @@
 import Image from "next/image";
 import { Libre_Bodoni } from "next/font/google";
+import { motion } from "motion/react";
 
 const libreBodoni = Libre_Bodoni({
   variable: "--font-libre-bodoni",
   subsets: ["latin"],
 });
 
+const items = [
+  {
+    title: "Campo Grafico",
+    hex: "#F4ECEA",
+  },
+  {
+    title: "Lo Studio Boggeri",
+    hex: "#D74C2F",
+  },
+  {
+    title: "Max Huber",
+    hex: "#D9AF01",
+  },
+  {
+    title: "Bruno Munari",
+    hex: "#6A6E71",
+  },
+  {
+    title: "Hans Neuberg",
+    hex: "#BC2729",
+  },
+];
+
 export default function Home() {
   return (
-    <div
-      className={`${libreBodoni.className} font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20`}
-    >
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+    <div className={`${libreBodoni.className} font-sans`}>
+      <main className="flex flex-col items-center ">
+        {items.map((item, i) => (
+          <section key={`section--${i}`} className="w-full h-lvh">
+            <div className="px-8 flex flex-col items-center justify-center gap-4 size-full">
+              <h2 className="text-xl font-bold">{item.title}</h2>
+              <div
+                className="aspect-square w-full max-w-[40rem]"
+                style={{ backgroundColor: item.hex }}
+              />
+              <div className="h-4 border border-foreground w-full flex relative">
+                {Array.from({ length: items.length }).map((_, j) => (
+                  <div
+                    key={`item-${j}`}
+                    className={`flex-1 border border-foreground`}
+                  />
+                ))}
+                <motion.div className={`bg-foreground absolute h-full w-1/5`} />
+              </div>
+            </div>
+          </section>
+        ))}
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
